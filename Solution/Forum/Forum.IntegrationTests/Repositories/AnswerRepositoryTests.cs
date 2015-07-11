47a99e421a0dfa8e4f4d05b510a3bfba77f2a844
James Heywood <jdheywood@yahoo.co.uk>
2015-07-11 18:03:01 +0100

    Further test coverage of AnswerRepository

--- Solution/Forum/Forum.IntegrationTests/Repositories/AnswerRepositoryTests.cs
+++ Solution/Forum/Forum.IntegrationTests/Repositories/AnswerRepositoryTests.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using Forum.Core.Helpers;
 using Forum.Domain.Contracts;
 using Forum.Domain.Entities;
 using NUnit.Framework;
 using Ploeh.AutoFixture;
+using Should;
 
 namespace Forum.IntegrationTests.Repositories
 {
     public class AnswerRepositoryTests
     {
         private readonly IAnswerRepository repository;
-        private const int NumberOfAnswers = 3;
+        private const int NumberOfAnswers = 4;
+        private const int UserIdOne = 123;
+        private const int UserIdTwo = 999;
 
         public AnswerRepositoryTests()
         {
@@ -23,9 +29,13 @@ namespace Forum.IntegrationTests.Repositories
         {
             var fixture = new Fixture();
 
-            var answers = fixture.CreateMany<Answer>();
+            var answers = new List<Answer>();
+            fixture.RepeatCount = NumberOfAnswers;
+            fixture.AddManyTo(answers);
 
-            CustomiseAndAddSampleAnswers(answers);
+            var question = fixture.Create<Question>();
+
+            CustomiseAndAddSampleAnswers(answers, question);
         }
 
         [TearDown]
@@ -48,20 +58,60 @@ namespace Forum.IntegrationTests.Repositories
             Assert.AreEqual(expectedNumber, actual.Count);
         }
 
+        [Test]
         public async void GetByUserId_WhenCalledReturnsAnswersForUser()
-        { }
+        {
+            const int expectedAnswerCount = 3;
+            string[] expectedAnswerIds = { "1", "2", "3" };
+            
+            var actual = await repository.GetByUserId(UserIdOne);
 
-        public async void GetNRecent_ReturnsRequestedNumberOrderedByDateDescending()
-        { }
+            actual.Count.ShouldEqual(expectedAnswerCount);
+            var countByExpectedIds = actual.Select(x => expectedAnswerIds.Contains(x.Id) && x.UserId == UserIdOne).Count();
+            countByExpectedIds.ShouldEqual(expectedAnswerCount);
+        }
 
+        [Test]
+        public async void GetNRecent_ReturnsLessThanRequestedNumberOrderedByDateDescendingDueToPublicAndRemovedFlags()
+        {
+            const int expectedCount = 1;
+            string[] expectedAnswerIds = { "1" };
+
+            var actual = await repository.GetNRecent(NumberOfAnswers);
+
+            var countByExpectedIds = actual.Select(x => expectedAnswerIds.Contains(x.Id) && x.UserId == UserIdOne).Count();
+            countByExpectedIds.ShouldEqual(expectedCount);            
+        }
+
+        [Test]
         public async void GetById_WhenCalledWithValidIdentifierReturnsAnswer()
-        { }
+        {
+            const string answerId = "2";
+            const string expectedText = "answer two...";
 
+            var actual = await repository.GetById(answerId);
+            actual.Text.ShouldEqual(expectedText);
+        }
+
+        [Test]
         public async void GetById_WhenCalledWithInvalidIdentifierReturnsNull()
-        { }
+        {
+            const string answerId = "999";
 
+            var actual = await repository.GetById(answerId);
+            actual.ShouldBeNull();
+        }
+
+        [Test]
         public async void GetByTextAndQuestionId_WhenMatchingAnswerInCollectionItIsReturned()
-        { }
+        {
+            const string questionId = "1";
+            const string text = "answer three...";
+
+            var actual = await repository.GetByTextAndQuestionId(text, questionId);
+            actual.Text.ShouldEqual(text);
+            actual.QuestionId.ShouldEqual(questionId);
+        }
 
         public async void GetByQuestionId_WhenMatchingAnswerInCollectionItIsReturned()
         { }
@@ -81,16 +131,52 @@ namespace Forum.IntegrationTests.Repositories
 
         #region Privates
 
-        private void CustomiseAndAddSampleAnswers(IEnumerable<Answer> answers)
+        private void CustomiseAndAddSampleAnswers(IEnumerable<Answer> answers, Question question)
         {
             var identifier = 1;
+
+            question.Id = identifier.ToString(CultureInfo.InvariantCulture);
+
             foreach (var answer in answers)
             {
                 var localAnswer = answer;
+                localAnswer.Id = identifier.ToString(CultureInfo.InvariantCulture);
+                localAnswer.QuestionId = question.Id;
+                localAnswer.DateTime = DateTime.Now.AddDays(-identifier);
+                localAnswer.Views = (identifier*10);
+
+                switch (identifier)
+                {
+                    case 1:
+                        localAnswer.UserId = UserIdOne;
+                        localAnswer.Public = true;
+                        localAnswer.Removed = true;
+                        localAnswer.Text = "answer one...";
+                        break;
+                    case 2:
+                        localAnswer.UserId = UserIdOne;
+                        localAnswer.Public = true;
+                        localAnswer.Removed = false;
+                        localAnswer.Text = "answer two...";
+                        break;
+                    case 3:
+                        localAnswer.UserId = UserIdOne;
+                        localAnswer.Public = false;
+                        localAnswer.Removed = true;
+                        localAnswer.Text = "answer three...";
+                        break;
+                    case 4:
+                        localAnswer.UserId = UserIdTwo;
+                        localAnswer.Public = false;
+                        localAnswer.Removed = false;
+                        localAnswer.Text = "answer four...";
+                        break;
+                }
+
+                identifier++;
 
                 AsyncHelpers.RunSync(() => repository.Add(localAnswer));
             }
-
         }
 
         #endregion
